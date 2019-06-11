86bdbb16e9a4a78a59b902f1fd181ff942cbb40c
annemck <annekh88@gmail.com>
2019-06-11 09:16:55 +0100

    added autowired annotation for raidRepository

--- src/test/java/com/codeclan/example/PirateService/PirateServiceApplicationTests.java
+++ src/test/java/com/codeclan/example/PirateService/PirateServiceApplicationTests.java
@@ -4,6 +4,7 @@ import com.codeclan.example.PirateService.models.Pirate;
 import com.codeclan.example.PirateService.models.Raid;
 import com.codeclan.example.PirateService.models.Ship;
 import com.codeclan.example.PirateService.repositories.PirateRepository;
+import com.codeclan.example.PirateService.repositories.RaidRepository;
 import com.codeclan.example.PirateService.repositories.ShipRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,6 +26,9 @@ public class PirateServiceApplicationTests {
 	@Autowired
 	ShipRepository shipRepository;
 
+	@Autowired
+	RaidRepository raidRepository;
+
 //	could do an @Before with pirateRepository.deleteAll() in it
 
 
@@ -43,23 +47,23 @@ public class PirateServiceApplicationTests {
 ////		remember it would add the same object again
 //	}
 
-	@Test
-	public void canCreatePirateAndShip() {
-		Ship ship1 = new Ship("The Flying Dutchman");
-		shipRepository.save(ship1);
-		Pirate pirate1 = new Pirate("Jack", "Sparrow", 32, ship1);
-		pirateRepository.save(pirate1);
-	}
-
 //	@Test
-//	public void pirateCanGoOnRaid() {
-//		Ship ship2 = new Ship("Marie Celeste");
-//		shipRepository.save(ship2);
-//		Pirate pirate2 = new Pirate("Joe", "Bloggs", 57, ship2);
-//		Raid raid = new Raid("Bermuda", 100);
-//		ArrayList<Raid> raids = new ArrayList<>();
-//		raids.add(raid);
-//		pirate2.setRaids(raids);
-//		pirateRepository.save(pirate2);
+//	public void canCreatePirateAndShip() {
+//		Ship ship1 = new Ship("The Flying Dutchman");
+//		shipRepository.save(ship1);
+//		Pirate pirate1 = new Pirate("Jack", "Sparrow", 32, ship1);
+//		pirateRepository.save(pirate1);
 //	}
+
+	@Test
+	public void pirateCanGoOnRaid() {
+		Ship ship2 = new Ship("Marie Celeste");
+		shipRepository.save(ship2);
+		Pirate pirate2 = new Pirate("Joe", "Bloggs", 57, ship2);
+		Raid raid = new Raid("Bermuda", 100);
+		ArrayList<Raid> raids = new ArrayList<>();
+		raids.add(raid);
+		pirate2.setRaids(raids);
+		pirateRepository.save(pirate2);
+	}
 }
